e31338584519e7a37136d5d6e0ea715472cc1878
jp <sharp.it.j@gmail.com>
2021-08-26 22:58:59 +0300

    main component

---
+++ src/app/main/main.component.ts
@@ -0,0 +1,41 @@
+import {HttpClient} from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map, filter, tap, take } from 'rxjs/operators';
+import { FakeData } from './main-models';
+
+@Component({
+  selector: 'app-main',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.scss']
+})
+export class MainComponent implements OnInit {
+
+  data: FakeData[] | undefined;
+
+  constructor(private httpClient: HttpClient) { }
+
+  ngOnInit(): void {
+    this.loadInitialData()
+      .subscribe(data => {
+        this.data = data;
+
+        ;
+      });
+  }
+
+  loadInitialData() : Observable<FakeData[]>{
+    return this.httpClient
+      .get<FakeData[]>('https://jsonplaceholder.typicode.com/users')
+        .pipe(
+          //map(x => x),
+          take(1),
+          map(x => x.filter(y => y.id > 5)),
+          tap(xy => console.log(xy.map(c => c.address.street).join(' -------> \n ')))
+            // tap(x => x
+            //   .filter(xx => xx.address.street !== 'Kulas Light')),
+            //     tap(x => x.map(x => x.id))
+        );
+  }
+
+}
